2f9cb803739deff47b6fc73123e67917a2c7915b
Ilya <180229@edu.psuti.ru>
2021-11-29 18:11:28 +0400

    Lab selection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,28 @@
-import util.Labs;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static util.Labs.*;
 
 public class Application {
 
     public static void main(String... args) {
-        Labs.lab5();
+        System.out.print("Введите номер лаб. работы: ");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            switch (scanner.nextInt()) {
+                case 1:
+                    lab1();
+                    break;
+                case 5:
+                    lab5();
+                    break;
+                default:
+                    throw new InputMismatchException();
+            }
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Ошибка. Повторите ввод");
+            main(args);
+        }
     }
 }
